206c9904f464392c5120cc28a415dce93ffdd4dc
Roman Skvazh <roman.skvazh@gmail.com>
2014-06-27 15:55:20 +0400

    ex11

---
+++ ex11/program.js
@@ -0,0 +1,10 @@
+var http = require('http');
+var fs = require('fs');
+
+var filename = process.argv[3];
+
+var server = http.createServer(function (req, res) {
+  res.writeHead(200, { 'content-type': 'text/plain' })
+  fs.createReadStream(filename).pipe(res)
+})
+server.listen(Number(process.argv[2]));
